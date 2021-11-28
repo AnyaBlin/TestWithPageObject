78f1ef60b6ea13fe0fdb21bb8706a115e6ce6f41
Ariisu <ariisutyan@gmail.com>
2021-11-29 00:21:05 +0300

    added config/properties

--- src/test/java/blin/ann/TestBase.java
+++ src/test/java/blin/ann/TestBase.java
@@ -4,20 +4,26 @@ import blin.ann.helpers.Attach;
 import blin.ann.pages.RegistrationPage;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class TestBase {
+import static java.lang.String.format;
 
+public class TestBase {
+    public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
     RegistrationPage registrationPage = new RegistrationPage();
 
     @BeforeAll
     static void setup() {
+        String url = System.getProperty("url", "selenoid.autotests.cloud/wd/hub/");
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = format("https://%s:%s@%s", credentials.login(),
+                credentials.password(), url);
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
